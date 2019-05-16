1dc20ed2a056be5c8dfc7552cc500b2e5d2ad844
Guilherme de Andrade <guilhermedeandrade@me.com>
2019-05-16 15:24:08 +0100

    lesson 2: completed

---
+++ lesson-2/index.js
@@ -0,0 +1,21 @@
+const Box = require("../utils/box");
+
+const moneyToFloat = str =>
+  Box(str)
+    .chain(s => s.replace(/\$/g, ""))
+    .chain(s => parseFloat(s));
+
+const percentToFloat = str =>
+  Box(str.replace(/\%/g, ""))
+    .chain(s => parseFloat(s))
+    .chain(n => n * 0.01);
+
+// note: notice that is also possible to pass something different than the arg itself to the box
+
+const applyDiscount = (price, discount) =>
+  moneyToFloat(price).fold(cost =>
+    percentToFloat(discount).fold(savings => cost - cost * savings)
+  );
+
+const finalPrice = applyDiscount("$5.00", "%20");
+console.log(finalPrice);
