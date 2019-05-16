919587b41c47e72b110bd7e20a47ffc2b964b7cf
Guilherme de Andrade <guilhermedeandrade@me.com>
2019-05-16 14:12:57 +0100

    lesson 1: completed

--- lesson-1/nextCharForNumberString.js
+++ lesson-1/nextCharForNumberString.js
@@ -1,10 +1,32 @@
-const nextCharForNumberString = str => {
-  const trimmed = str.trim();
-  const number = parseInt(trimmed);
-  const nextNumber = number + 1;
-  return String.fromCharCode(nextNumber);
-};
+// const nextCharForNumberString = str => {
+//   const trimmed = str.trim();
+//   const number = parseInt(trimmed);
+//   const nextNumber = number + 1;
+//   return String.fromCharCode(nextNumber);
+// };
+
+// const nextCharForNumberString = str =>
+//   String.fromCharCode(parseInt(str.trim()) + 1);
+
+// const nextCharForNumberString = str =>
+//   [str]
+//     .map(s => s.trim())
+//     .map(trimmedString => parseInt(trimmedString))
+//     .map(n => n + 1)
+//     .map(nextNumber => String.fromCharCode(nextNumber));
+
+const Box = x => ({
+  chain: f => Box(f(x)),
+  fold: f => f(x),
+  inspect: () => `Box(${x})`
+});
+
+const nextCharForNumberString = str =>
+  Box(str)
+    .chain(s => s.trim())
+    .chain(s => parseInt(s))
+    .chain(n => n + 1)
+    .fold(n => String.fromCharCode(n));
 
 const result = nextCharForNumberString(" 64 ");
-
 console.log(result);
