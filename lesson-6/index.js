b35132a9c3c41b5b87d65910cb0bfbacb896e85e
Guilherme de Andrade <guilhermedeandrade@me.com>
2019-05-21 17:50:20 +0100

    chore: lesson-6 clean up

--- lesson-6/index.js
+++ lesson-6/index.js
@@ -3,15 +3,7 @@
 const firstResult = require("./exercise-1");
 const secondResult = require("./exercise-2");
 const thirdResult = require("./exercise-3");
-// const fourthResult = require("./exercise-4");
-// const fifthResult = require("./exercise-5");
 
-const results = [
-  firstResult,
-  secondResult,
-  thirdResult
-  // fourthResult,
-  // fifthResult
-];
+const results = [firstResult, secondResult, thirdResult];
 
 console.table(results, ["x"]);
